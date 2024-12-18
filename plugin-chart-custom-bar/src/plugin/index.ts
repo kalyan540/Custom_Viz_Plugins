18b553029a6251a7ed22b5e68e5c5bf315138a56
Kalyan540 <kalyanroczzzz@gmail.com>
2024-12-18 16:57:54 +0530

    fix: minor path issue

--- plugin-chart-custom-bar/src/plugin/index.ts
+++ plugin-chart-custom-bar/src/plugin/index.ts
@@ -21,9 +21,9 @@ import buildQuery from './buildQuery';
 import controlPanel from './controlPanel';
 import transformProps from './transformProps';
 import thumbnail from '../images/thumbnail.png';
-import example1 from './images/Bar1.png';
-import example2 from './images/Bar2.png';
-import example3 from './images/Bar3.png';
+import example1 from '../images/Bar1.png';
+import example2 from '../images/Bar2.png';
+import example3 from '..images/Bar3.png';
 import {
   EchartsTimeseriesChartProps,
   EchartsTimeseriesFormData,
