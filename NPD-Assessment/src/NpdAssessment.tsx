64d9d1174491cc96f39fb057b01a8dc855a46828
Kalyan540 <kalyanroczzzz@gmail.com>
2025-01-13 02:23:01 +0530

    Adjust scrollHeight calculation in NpdAssessment for improved layout

--- NPD-Assessment/src/NpdAssessment.tsx
+++ NPD-Assessment/src/NpdAssessment.tsx
@@ -456,7 +456,7 @@ export default function NpdAssessment(props: NpdAssessmentProps) {
           globalFilter={globalFilter}
           header={header}
           scrollable
-          scrollHeight={height - 150 + "px"}
+          scrollHeight={height - 100 + "px"}
 
         >
           {columns.map((col) => (
