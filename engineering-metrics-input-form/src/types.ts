a6d08220d9d4de349494f6f627432132f3dfc65d
kotnisai <kotnisai@gmail.com>
2025-02-18 17:15:27 +0530

    remove legend form data

--- engineering-metrics-input-form/src/types.ts
+++ engineering-metrics-input-form/src/types.ts
@@ -122,7 +122,6 @@ export type EngineeringMetricsFormData = QueryFormData & {
 };
 
 export const DEFAULT_FORM_DATA: Partial<EngineeringMetricsFormData> = {
-  ...DEFAULT_LEGEND_FORM_DATA,
   groupby: [],
   rowLimit: 10,
   minVal: null,
