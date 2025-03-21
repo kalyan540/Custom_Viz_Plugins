44ea7294ffcbe4c008dbde69f1d6c41a4e2fff73
Tanuja222 <tanujakolasani@gmail.com>
2025-03-21 16:37:52 +0530

    fix

--- FlowBuilder/src/FlowBuilder.tsx
+++ FlowBuilder/src/FlowBuilder.tsx
@@ -305,7 +305,13 @@ export default function FlowBuilder(props: FlowBuilderProps) {
                 msg.params = [
                   ${workflow_id}, // workflow_id
                   JSON.stringify(requestData), // Ensure request_data is properly stringified
-                  msg.payload.status || "Pending", // status
+
+                  if(index === managers.length - 1){
+                  if(msg.payload.status=="Approved"){
+                  msg.payload.status = "Completed", // status
+                    }  
+                  }
+                  
                   ${index + 1}, // current_level
                   ${managers.length}, // total_levels
                   msg.payload.requestid// requestid
