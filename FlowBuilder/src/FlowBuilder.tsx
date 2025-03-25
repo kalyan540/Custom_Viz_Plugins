ccf85bb6f0522ec9d292ad3377f9a6f40ec5a531
Tanuja222 <tanujakolasani@gmail.com>
2025-03-25 12:08:58 +0530

    fix

--- FlowBuilder/src/FlowBuilder.tsx
+++ FlowBuilder/src/FlowBuilder.tsx
@@ -206,7 +206,7 @@ export default function FlowBuilder(props: FlowBuilderProps) {
       workflow.push({
           "id": "workflow_approval",
           "type": "tab",
-          "label": "workflowName"
+          "label": workflowName
 
       });
     
